91f4ed52df419f73123fc35882089d7adda633cc
pokerpoke <j824544269@gmail.com>
2018-08-28 10:06:01 +0800

    removeNthFromEnd

---
+++ src/removeNthFromEnd.cc
@@ -0,0 +1,48 @@
+/**
+ * 
+ * Copyright (c) 2018 NUAA Jiang Yang
+ * 
+ * @file
+ * @author   姜阳 (j824544269@gmail.com)
+ * @date     2018-08
+ * @brief    
+ * @version  0.0.1
+ * 
+ * Last Modified:  2018-08-28
+ * Modified By:    姜阳 (j824544269@gmail.com)
+ * 
+ */
+#include "headers.h"
+
+int main()
+{
+    List l;
+    for (size_t i = 0; i < 10; i++)
+        l.insert(i);
+
+    l.print();
+
+    int n = 3;
+
+    ListNode *slow = l.head;
+    ListNode *fast = l.head;
+
+    if (fast == nullptr)
+        return head->next;
+
+    for (size_t i = 0; i < n; i++)
+        fast = fast->next;
+
+    while (fast->next != nullptr)
+    {
+        slow = slow->next;
+        fast = fast->next;
+    }
+
+    slow->next = slow->next->next;
+
+    l.print();
+
+    getchar();
+    return 0;
+}

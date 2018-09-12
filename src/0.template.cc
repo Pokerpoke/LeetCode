285af7df8501d0845b6cb1550ad47d6e9c9545b9
pokerpoke <j824544269@gmail.com>
2018-09-12 16:51:21 +0800

    Update template

---
+++ src/0.template.cc
@@ -0,0 +1,43 @@
+/**
+ * 
+ * Copyright (c) 2018 NUAA Jiang Yang
+ * 
+ * @file
+ * @author   姜阳 (pokerpoke@qq.com)
+ * @date     2018-08
+ * @brief    
+ * @version  0.0.1
+ * 
+ * Last Modified:  2018-09-12
+ * Modified By:    姜阳 (pokerpoke@qq.com)
+ * 
+ */
+#include "headers.h"
+
+class Solution
+{
+  public:
+    bool isValid(string s)
+    {
+        return true;
+    }
+};
+
+TEST(isValid, isValid)
+{
+    Solution s;
+    EXPECT_TRUE(s.isValid(""));
+    EXPECT_TRUE(s.isValid("()"));
+    EXPECT_FALSE(s.isValid(")"));
+    EXPECT_TRUE(s.isValid("[]"));
+    EXPECT_TRUE(s.isValid("()[]{}"));
+    EXPECT_FALSE(s.isValid("(])"));
+    EXPECT_FALSE(s.isValid("([)]"));
+}
+
+int main(int argc, char **argv)
+{
+    ::testing::InitGoogleTest(&argc, argv);
+
+    return RUN_ALL_TESTS();
+}
